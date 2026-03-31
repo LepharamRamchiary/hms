0b3ec7d2aa1d938f8f061d3145a9d1a54acfa640
LepharamRamchiary <lepharamchiary@gmail.com>
2026-03-31 15:51:23 +0530

    added mongodb connection compass

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MOGODB_URI)
+  .then(() => console.log("mongodb connetd bro"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
